de3074c1bff688577055cb451d5d4dacd809eddb
optimalname <spasovevgeniy@gmail.com>
2021-06-28 00:29:03 +0300

    вторая попытка понять POM. 5.4_step9

--- module_5/pages/locators.py
+++ module_5/pages/locators.py
@@ -4,11 +4,6 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form"),
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form"),
-    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
-
-class ProductPageLocators():
-    ADDING_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
-    PRODUCT_IN_BASKET = (By.XPATH, "//div[@id='messages']/div[1]/div[1]/strong")
-    PRICE_OF_PRODUCT = (By.XPATH, "//div[@id='messages']/div[3]//p[1]")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form1")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form1")
+    LOGIN_URL = (By.CSS_SELECTOR, "#login_link1")
