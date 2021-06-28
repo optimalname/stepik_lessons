c381b63550745c0ad6ee96072ce5fb66b108a35f
optimalname <spasovevgeniy@gmail.com>
2021-06-28 22:30:52 +0300

    вторая попытка понять POM. 5.5_step2

--- module_5/pages/product_page.py
+++ module_5/pages/product_page.py
@@ -6,11 +6,17 @@ class ProductPage(BasePage):
     def should_be_product_page(self):
         self.should_be_product_in_basket()
         self.should_be_product_price()
+        self.should_be_button_add_to_basket()
+
+    def should_be_button_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_BASKET), "Button add to basket is not presented"
+
+    def add_product_to_basket(self):
+        add_product_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        add_product_to_basket.click()
+
+    def should_be_product_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRICE_OF_PRODUCT), "Messege with price of product is not presented"
 
     def should_be_product_in_basket(self):
-        assert self.is_element_present(*ProductPageLocators,)
-
-    def go_to_product_page(self):
-        button = self.browser.find_element(*ProductPageLocators.ADDING_TO_BASKET)
-        button.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_IN_BASKET), "Messege with product in basket is not presented"
--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -1,28 +1,13 @@
-import pytest
-from selenium import webdriver
 from .pages.product_page import ProductPage
-from .pages.locators import ProductPageLocators
 
-@pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-@pytest.mark.parametrize('link_param', ["?promo=newYear"])
 class TestProductPage:
-    def test_guest_can_go_to_product_page(self, browser, link_param):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/{link_param}/"
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser,
                         link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open() # открываем страницу
-
-    def test_guest_can_add_product_to_basket(self, browser, link_param):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/{link_param}/"
-        page = ProductPage(browser,
-                        link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open() # открываем страницу
-        page.should_be_add_to_cart_button() # проверяем что есть кнопка добавления в корзину
-        page.ADDING_TO_BASKET() # жмем кнопку добавить в корзину
-        page.should_be_success_messege() # проверяем что есть сообщение с нужным текстом
+        page.should_be_button_add_to_basket() # проверяем что есть кнопка добавления в корзину
+        page.add_product_to_basket() # жмем кнопку добавить в корзину
+        page.solve_quiz_and_get_code() # обработка alert
+        page.should_be_product_price() # проверяем что есть сообщение с нужным текстом
+        page.should_be_product_in_basket()  # проверяем что есть сообщение с нужным текстом
